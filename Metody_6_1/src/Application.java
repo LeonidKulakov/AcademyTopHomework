831132606cd60310f1e4bb1aa46887538a5882db
LeonidKulakov <107943543+LeonidKulakov@users.noreply.github.com>
2022-09-14 21:26:14 +0300

    Akad_Top_homework_14_09

--- Metody_6_1/src/Application.java
+++ Metody_6_1/src/Application.java
@@ -9,8 +9,14 @@ public class Application {
 
     private void start() {
         System.out.println("Задача №1");
-        Task1 task1 = new Task1();
-        task1.print();
+        int[][] a = {{5, 7, 3}, {7, 0, 1}, {8, 1, 2}};
+        int[][] b = {{1, 2, 3}, {3, 4, 5}, {5, 6, 7}};
+        Task1 task1 = new Task1(a);
+        Task1 task11 = new Task1(b);
+        task1.print(task1.makeOneMatrix(4));
+        task1.print(task1.makeZeroMatrix(4));
+        task1.print(task1.addMatrix(task11));
+        task1.print(task1.multiplication(2));
         System.out.println("\nЗадача №2");
         Task2 task2 = new Task2();
         task2.start();
--- Metody_6_1/src/Task1.java
+++ Metody_6_1/src/Task1.java
@@ -1,8 +1,66 @@
 public class Task1 {
 
-    public void print(){
-        System.out.println("Сначала подучу как матрицы треугольником считать и обязательно сделаю это шикарнейшее задание." +
-                " Вот только сдам ЕГЭ \n (А то 18 лет назад, когда поступал в институт его не было), потом поступлю в институт," +
-                " закончу первый курс \n и обязательно сделаю, вот прям честное слово!");
+    private int[][] array;
+
+    public Task1() {
+    }
+
+    public Task1(int[][] array) {
+        this.array = array;
+    }
+
+    public int[][] makeOneMatrix(int a) {
+        int[][] arr = new int[a][a];
+        for (int i = 0; i < a; i++) {
+            for (int j = 0; j < a; j++) {
+                if (i == j) {
+                    arr[i][j] = 1;
+                } else {
+                    arr[i][j] = 0;
+                }
+            }
+        }
+        return arr;
+    }
+
+    public int[][] makeZeroMatrix(int a) {
+        int[][] arr = new int[a][a];
+        for (int i = 0; i < a; i++) {
+            for (int j = 0; j < a; j++) {
+                arr[i][j] = 0;
+            }
+        }
+        return arr;
+    }
+
+
+    public void print(int[][] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[0].length; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    public int[][] addMatrix(Task1 arr) {
+        int[][] arrSum = new int[this.array.length][this.array[0].length];
+        for (int i = 0; i < this.array.length; i++) {
+            for (int j = 0; j < this.array[0].length; j++) {
+                arrSum[i][j] = array[i][j] + arr.array[i][j];
+            }
+        }
+        return arrSum;
+    }
+
+    public int[][] multiplication(int a) {
+        int[][] arrSum = new int[this.array.length][this.array[0].length];
+        for (int i = 0; i < this.array.length; i++) {
+            for (int j = 0; j < this.array[0].length; j++) {
+                arrSum[i][j] = array[i][j] * a;
+            }
+        }
+        return arrSum;
     }
 }
